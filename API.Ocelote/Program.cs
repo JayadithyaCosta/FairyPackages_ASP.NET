a9846e1ce90765bfd4bb7932b17ef9facccef31b
Jay Costa <it17088138@my.sliit.lk>
2023-04-16 13:02:57 +0530

    ocelot gateway error fix and running

--- API.Ocelote/Program.cs
+++ API.Ocelote/Program.cs
@@ -1,6 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.MapGet("/", () => "Hello World!");
+namespace API.Ocelot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.AddJsonFile("ocelot.json"); // Add your Ocelot configuration file here
+                    })
+                    .ConfigureServices(services =>
+                    {
+                        services.AddControllers(); // Register controllers
+                        services.AddOcelot(); // Add Ocelot services
+                    })
+                    .Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                        });
+
+                        app.UseOcelot().Wait(); // Use Ocelot middleware
+                    });
+                });
+    }
+}
